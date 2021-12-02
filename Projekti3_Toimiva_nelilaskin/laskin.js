b77283ca75fff7f062ef7dd45a09c22eab22fc6b
Furkan Digiroglu <90837469+digiroglufurkan@users.noreply.github.com>
2021-12-02 20:45:49 +0200

    uusi koodi logikki ja pieni muokkaus css

--- Projekti3_Toimiva_nelilaskin/laskin.js
+++ Projekti3_Toimiva_nelilaskin/laskin.js
@@ -1,49 +1,32 @@
 
 var luku1="";
-var luku2;
+var luku2 =[];
 var tulos= 0;
-var lasketus ;
-var ensimainen = true;
 function lukusyotta(a)
 {
     luku1 += a ;
     document.getElementById("input1").value = luku1.toString();
 }
-function laske(a)
+function laske(b)
 {
-let intluku = parseInt(luku1);
-lasketus = (a < 19 )? a:lasketus;
-switch (a) {
-  case 11:
-  tulos += parseInt(luku1);
-  luku1 ="";
-  document.getElementById("input1").value ="";
-  break;
-  case 12:
-  tulos = (ensimainen)? intluku :tulos - intluku;
-  luku1 ="";
-  ensimainen = false;
-  document.getElementById("input1").value ="";
-  break;
-  case 13:
-  tulos = (ensimainen)? intluku: tulos*intluku;
-  luku1 ="";
-  ensimainen = false;
-  document.getElementById("input1").value ="";
-  break;
-  case 14:
-  tulos = (ensimainen)? intluku: tulos/intluku;
-  luku1 ="";
-  document.getElementById("input1").value ="";
-  break;
-  case 15:
-  luku1 ="";
-  document.getElementById("input1").value ="";
-  break;
-  case 19:
-  laske(lasketus);
-  luku1 ="";
-  return  document.getElementById("input1").value = tulos.toString();
-  break;
+  luku2.push(document.getElementById("input1").value);
+  luku1="";
+  luku2.push((b =='%')? '*1/100':b);
+  document.getElementById("input1").value = "";
 }
+function on()
+{
+  luku2.push(document.getElementById("input1").value);
+  luku1="";
+  tulos = eval(luku2.toString().replace(/,/g, ''));
+  document.getElementById("input1").value= tulos.toString();
+  luku2=[];
+  tulos=0;
+}
+function poista(c)
+{
+   luku1="";
+   luku2 = (c=='C')? luku2=[]: luku2 ;
+   tulos= 0;
+   document.getElementById("input1").value = "";
 }
