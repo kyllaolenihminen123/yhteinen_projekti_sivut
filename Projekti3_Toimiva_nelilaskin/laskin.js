894e0f20730e9fe9e180d05f27153c2d7a84fff4
Furkan Digiroglu <90837469+digiroglufurkan@users.noreply.github.com>
2021-12-02 10:38:23 +0200

    Htmlja css melkein valmis mutta koodi jatkaa

--- Projekti3_Toimiva_nelilaskin/laskin.js
+++ Projekti3_Toimiva_nelilaskin/laskin.js
@@ -1,1 +1,24 @@
-var
+
+var luku1="";
+var luku2;
+var tulos= 0;
+var lasketus ;
+function lukusyotta(a)
+{
+    luku1 += a ;
+    document.getElementById("input1").value = luku1.toString();
+}
+function laske(a)
+{
+lasketus ="";
+switch (a) {
+  case 11:
+    tulos += parseInt(luku1);
+    luku1 ="";
+    return document.getElementById("input1").value="";
+    break;
+  case 12:
+    return  document.getElementById("input1").value = tulos.toString();
+    break;
+}
+}
