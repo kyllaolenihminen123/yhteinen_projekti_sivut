0c37080945d78d2fa36d51d131bfaf41d30f92d2
kyllaolenihminen123 <90385541+kyllaolenihminen123@users.noreply.github.com>
2021-12-09 14:35:23 +0200

    Projekti 1b tehty

--- Projekti1B_ Kirjautumislomake_käyttäjätarinoiden_perusteella/koodi.js
+++ Projekti1B_ Kirjautumislomake_käyttäjätarinoiden_perusteella/koodi.js
@@ -1,9 +1,20 @@
 function lähetä() {
   var nimi=document.getElementById('nimi').value;
-  if (nimi == "" || salasana == "" ||)   {
-    alert("Et ole täyttänyt kaikkia kenttiä.");
-    return;
+  var ika=document.getElementById('ika').value;
+  var email=document.getElementById('email').value;
+  if(nimi.length < 2) {
+    alert("Tarkista nimi kenttä.");
+    return false;
+  }
+  if (ika <18 || ika >64) {
+    alert("Tarkista ikä kenttä.");
+    return false;
+  }
+  if (email < /^[^\s@]+@[^\s@]+\.[^\s@]+$/) {
+    alert("Tarkista sähköposti kenttä");
+    return false;
   }
   document.getElementById("kirjautumislomake").submit();
   alert("Lähetetty!");
+  return true;
 }
