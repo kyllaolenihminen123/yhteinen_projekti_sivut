7fad5f433c9c245d5d9bd47001ab6696b8b164fc
kyllaolenihminen123 <tatu.puhakka@edu.keuda.fi>
2022-02-14 03:41:44 +0200

    Jatkoa

--- Projekti2A_5_Kysymystä_askel_askeleelta/script.js
+++ Projekti2A_5_Kysymystä_askel_askeleelta/script.js
@@ -1,27 +1,26 @@
+var laske = 0;
+var nolla = false;
 // Tarkistaa kysymyksien valintojen arvot.
-var laske =0;
-var nolla=false;
 function tarkista() {
- if (nolla) {
-   nolla =false;
-   window.location.reload();
+  if (nolla) {
+    nolla = false;
+    window.location.reload();
   }
-  vastaa("b",1);
-  vastaa("a",2);
-  vastaa("d",3);
-  vastaa("b",4);
-  vastaa("b",5);
-document.getElementById("tulos").innerHTML= laske + "/5" + " Oikein Vastaa";
- nolla=true;
+  vastaa("b", 1);
+  vastaa("a", 2);
+  vastaa("d", 3);
+  vastaa("b", 4);
+  vastaa("b", 5);
+  document.getElementById("tulos").innerHTML = laske + "/5" + " Oikein";
+  nolla = true;
 }
-function vastaa(oikeinvastaa, kysymys)
-{
+
+function vastaa(oikeinvastaa, kysymys) {
   var k = document.forms["lomake"]["k" + kysymys.toString()].value;
-  document.getElementById(k + kysymys.toString()).style.color= "red";
-  if (oikeinvastaa == k)
-  {
-  document.getElementById(oikeinvastaa + kysymys.toString()).style.color= "green";
-  laske++;
+  document.getElementById(k + kysymys.toString()).style.color = "red";
+  if (oikeinvastaa == k) {
+    document.getElementById(oikeinvastaa + kysymys.toString()).style.color = "green";
+    laske++;
   }
 
 }
