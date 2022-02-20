c7fa3d9ebd5b7d2d503f60021e7f48210d715950
Furkan Digiroglu <90837469+digiroglufurkan@users.noreply.github.com>
2022-02-20 23:04:33 +0200

    teen koodin osa

--- Projekti6_Tallentaa_paikalliselle_koneelle/koodi.js
+++ Projekti6_Tallentaa_paikalliselle_koneelle/koodi.js
@@ -1,1 +1,36 @@
-v
+
+
+function lataa()
+{
+  document.getElementById("vadita").innerHTML = "";
+  var auto = "";
+  auto = document.getElementById("merkki").value;
+  auto += " " + document.getElementById("malli").value ;
+  auto += " " + document.getElementById("hinta").value;
+  let tunusid = document.getElementById("id1").value;
+  if (tunusid == "")
+  {
+    document.getElementById("vadita").innerHTML = " ole hyvä  ja syötä ID";
+  }
+  else {
+    window.localStorage.setItem(tunusid, auto);
+  }
+}
+function hae1()
+{
+
+  let a = document.getElementById("hae").value;
+  if ( a !== "" )
+  {
+  document.getElementById("tulos1").innerHTML= localStorage[a];
+}
+}
+function poista()
+{
+  let a = document.getElementById("tpoista").value;
+  if ( a !== "")
+  {
+    localStorage.removeItem(a);
+  }
+  document.getElementById("tpoista").value = "";
+}
